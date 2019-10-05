3b4a8bd596487a1162137fa351e0dbf1f1cf9845
carraghr <carraghr@gmail.com>
2019-10-05 23:21:00 +0100

    Update App.js
    Added basic layout and implmentation for messagelist, text input and message room list

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RoomList from './components/RoomList'
+
+import RoomList from './components/JoinRoomForm'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import JoinRoomForm from './components/RoomList'
 
 import './index.css';
 
 class App extends React.Component{
+	
 	constructor(){
 		super();
 
 		this.state = {
 			messages:[],
 		}
-
+		this.sendMessage = this.sendMessage.bind(this);
 	}
 
 	componentDidMount(){
 		//get api url,
-		/*
-		this.setState({
-					messages: [... this.state.messages, messages]
-				})
-		*/
+		//connect to api 
+		/**
+		 * this.currentUser should be defined here
+		 */
 		
 	}
 
 	sendMessage(text){
-
+		this.currentUser.sendMessage({
+			text,
+			roomId: "",
+		});
 	}
 
 	render(){
 		return(
 			<div className="app">
-				<RoomList/>
+				<RoomList rooms={[...this.state.joinableRooms, ... this.state.joinedRooms]}/>
 				<MessageList messages={this.state.messages}/>
-				<SendMessageForm sendMessage/>
+				<SendMessageForm sendMessage={this.sendMessage}/>
 				<JoinRoomForm/>
 			</div>
 		);
 	}
 }
+
+export default App
